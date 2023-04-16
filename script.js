699ef6fabb259c7e669836ae031bcead8c875ed2
Kajal Kumari <114283323+kajalkumari2507@users.noreply.github.com>
2023-04-16 20:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your code here
+describe("Feedback Form Test", () => {
+  const baseUrl = "http://localhost:3000"; // change this to your app's base URL
+
+  it("should have all necessary form elements", () => {
+    cy.visit(baseUrl + "/main.html");
+
+    cy.contains("Feedback Form").should("be.visible");
+
+    cy.get("form").should("exist");
+
+    cy.get("input#name")
+      .should("have.attr", "minlength", "3")
+      .should("have.attr", "maxlength", "50");
+    cy.get("label[for='name']").should("be.visible");
+
+    cy.get("input#portfolio[type='url']").should("exist");
+    cy.get("label[for='portfolio']").should("be.visible");
+
+    cy.get("input#favColor[type='color']").should("exist");
+    cy.get("label[for='favColor']").should("be.visible");
+
+    cy.get("input#likeness[type='range']")
+      .should("have.attr", "min", "1")
+      .should("have.attr", "max", "100");
+    cy.get("label[for='likeness']").should("be.visible");
+  });
+
+  it("should have a 'post' method on the form", () => {
+    cy.get("form")
+      .should("have.attr", "method")
+      .and("match", /post/i);
+  });
+});
